472b10cbdd0895e225d626f95e08b78de6fd8d88
Maks <293405@studnet.pwr.edu.pl>
2025-11-21 19:03:18 +0100

    Poprawa błędów i ulepszenie

--- Lista 3/Zadanie 1.c
+++ Lista 3/Zadanie 1.c
@@ -4,78 +4,77 @@
 
 int main() {
     // a
-    // {
-    //     int n;
-    //     printf("Ile chcesz wyrazow w tablicy: ");
-    //     scanf("%d", &n);
-    //
-    //     double A[n];
-    //     for (int i=0; i<n; i++) {
-    //         scanf("%lg", &A[i]);
-    //     }
-    //     printf("A = {");
-    //     for (int i=0; i<n; i++) {
-    //         printf("%g", A[i]);
-    //         if (i<n-1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("}");
-    //
-    //     printf("\n");
-    //
-    //     double B[n];
-    //     srand(time(0));
-    //     printf("B = {");
-    //     for (int i=0; i<n; i++) {
-    //         B[i]=(float)rand()/RAND_MAX*100;
-    //         printf("%g", B[i]);
-    //         if (i<n-1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("}");
-    //
-    //     printf("\n");
-    //
-    //     double C[n];
-    //     printf("C = {");
-    //     for (int i=0; i<n; i++) {
-    //         C[i]=A[i]*B[i];
-    //         printf("%g", C[i]);
-    //         if (i<n-1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("}");
-    // }
+    {
+        int n;
+        printf("Ile chcesz wyrazow w tablicy: ");
+        scanf("%d", &n);
+
+        double A[n];
+        for (int i=0; i<n; i++) {
+            printf("Podaj %d wyraz tablicy: ", (i+1));
+            scanf("%lg", &A[i]);
+        }
+        printf("A = {");
+        for (int i=0; i<n; i++) {
+            printf("%g", A[i]);
+            if (i<n-1) {
+                printf(", ");
+            }
+        }
+        printf("}\n");
+
+        double B[n];
+        srand(time(0));
+        printf("B = {");
+        for (int i=0; i<n; i++) {
+            B[i]=(double)rand()/RAND_MAX*100;
+            printf("%g", B[i]);
+            if (i<n-1) {
+                printf(", ");
+            }
+        }
+        printf("}\n");
+
+        double C[n];
+        printf("C = {");
+        for (int i=0; i<n; i++) {
+            C[i]=A[i]*B[i];
+            printf("%g", C[i]);
+            if (i<n-1) {
+                printf(", ");
+            }
+        }
+        printf("}\n\n");
+    }
 
     // b
-    // {
-    //     int D[10][3];
-    //     int silnia(int n);
-    //     int fib(int n);
-    //     printf("n        SIL  FIB\n");
-    //     for (int i=0; i<10; i++) {
-    //         D[i][0]=i+1;
-    //         D[i][1]=silnia(i);
-    //         D[i][2]=fib(i);
-    //         }
-    //
-    //     for (int i=0; i<10; i++){
-    //         printf("%-2d  %8d %4d\n", D[i][0], D[i][1], D[i][2]);
-    //     }
-    //
-    // }
+    {
+        int D[10][3];
+        int silnia(int n);
+        int fib(int n);
+        printf("n        SIL  FIB\n");
+        for (int i=0; i<10; i++) {
+            D[i][0]=i+1;
+            D[i][1]=silnia(i);
+            D[i][2]=fib(i);
+            }
+
+        for (int i=0; i<10; i++){
+            printf("%-2d  %8d %4d\n", D[i][0], D[i][1], D[i][2]);
+        }
+        printf("\n");
+    }
 
 
     // c
     {
-        void A();
-        void B();
-        void C();
-        void wypisanie();
-        int n=5;
+        void A(double A[], int n);
+        void B(double B[], int n);
+        void C(double A[], double B[], double C[], int n);
+        void wypisanie(double A[], double B[], double C[], int n);
+        int n;
+        printf("Ile chcesz wyrazow w tablicy: ");
+        scanf("%d", &n);
         double tabA[n], tabB[n], tabC[n];
         A(tabA, n);
         B(tabB, n);
@@ -105,14 +104,16 @@ int fib(int n) {
 }
 
 void A(double A[], int n) {
-    for (int i=0; i<n; i++)
+    for (int i=0; i<n; i++) {
+        printf("Podaj %d wyraz tablicy: ", (i+1));
         scanf("%lg", &A[i]);
+    }
 }
 
 void B(double B[], int n) {
     srand(time(0));
     for (int i=0; i<n; i++)
-        B[i]=(float)rand()/RAND_MAX*100;
+        B[i]=(double)rand()/RAND_MAX*100;
 }
 
 void C(double A[], double B[], double C[], int n) {

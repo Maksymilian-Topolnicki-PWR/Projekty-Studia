538f53b9887d653a9f1c4539c0ba867fe65537c9
Maks <293405@studnet.pwr.edu.pl>
2025-11-24 22:35:40 +0100

    Zadania na kolosa

--- Zadania na kolokwium/Czesc 1.c
+++ Zadania na kolokwium/Czesc 1.c
@@ -1,3 +1,56 @@
-//
-// Created by Maksymilian on 24.11.2025.
-//
+#include <stdio.h>
+
+int main() {
+    // // a
+    // {
+    //     int A[4];
+    //     int ile2=0, ile3=0, ile4=0;
+    //     printf("Podaj 4 liczby calkowite\n");
+    //     printf("Liczba 1: ");
+    //     for (int i=0; i<4; i++) {
+    //         printf("Podaj liczbe 1: ");
+    //         scanf("%d", &A[i]);
+    //     }
+    //
+    //     for (int i=0; i<A[i]; i++) {
+    //
+    //     }
+    //
+    //
+    //
+    //     printf("Liczba 2: %d\n", ile2);
+    //     printf("Liczba 3: %d\n", ile3);
+    //     printf("Liczba 4: %d", ile4);
+    //
+    // }
+
+
+    // // b
+    // {
+    //     char znak;
+    //     int t=0, a=0, o=0;
+    //
+    //     printf("Wpisuj znaki: ");
+    //
+    //     while (1) {
+    //         scanf("%c", &znak);
+    //         if (znak=='t')
+    //             t++;
+    //         if (znak=='a')
+    //             a++;
+    //         if (znak=='o')
+    //             o++;
+    //
+    //         if (t>=2 && a>=1 && o>=1) {
+    //             printf("Mozna zlozyc slowo tato!");
+    //             break;
+    //         }
+    //     }
+    //
+    // }
+
+    // c
+    {
+
+    }
+}

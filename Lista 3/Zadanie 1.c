c4ee22b2834aac544a4d614593730be7ddc63bee
Maks <293405@studnet.pwr.edu.pl>
2025-11-24 22:03:33 +0100

    Zadanie dodatkowe +

--- Lista 3/Zadanie 1.c
+++ Lista 3/Zadanie 1.c
@@ -4,95 +4,95 @@
 #include <time.h>
 
 int main() {
-    // // a
-    // {
-    //     int n;
-    //     printf("Ile chcesz wyrazow w tablicy: ");
-    //     scanf("%d", &n);
-    //
-    //     double A[n];
-    //     for (int i=0; i<n; i++) {
-    //         printf("Podaj %d wyraz tablicy: ", (i+1));
-    //         scanf("%lg", &A[i]);
-    //     }
-    //     printf("A = {");
-    //     for (int i=0; i<n; i++) {
-    //         printf("%g", A[i]);
-    //         if (i<n-1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("}\n");
-    //
-    //     double B[n];
-    //     srand(time(0));
-    //     printf("B = {");
-    //     for (int i=0; i<n; i++) {
-    //         B[i]=(double)rand()/RAND_MAX*100;
-    //         printf("%g", B[i]);
-    //         if (i<n-1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("}\n");
-    //
-    //     double C[n];
-    //     printf("C = {");
-    //     for (int i=0; i<n; i++) {
-    //         C[i]=A[i]*B[i];
-    //         printf("%g", C[i]);
-    //         if (i<n-1) {
-    //             printf(", ");
-    //         }
-    //     }
-    //     printf("}\n\n");
-    // }
-    //
-    // // b
-    // {
-    //     int D[10][3];
-    //     int silnia(int n);
-    //     int fib(int n);
-    //     printf("n        SIL  FIB\n");
-    //     for (int i=0; i<10; i++) {
-    //         D[i][0]=i+1;
-    //         D[i][1]=silnia(i);
-    //         D[i][2]=fib(i);
-    //         }
-    //
-    //     for (int i=0; i<10; i++){
-    //         printf("%-2d  %8d %4d\n", D[i][0], D[i][1], D[i][2]);
-    //     }
-    //     printf("\n");
-    // }
-    //
-    //
-    // // c
-    // {
-    //     void A(double A[], int n);
-    //     void B(double B[], int n);
-    //     void C(double A[], double B[], double C[], int n);
-    //     void wypisanie(double A[], double B[], double C[], int n);
-    //     int n;
-    //     printf("Ile chcesz wyrazow w tablicy: ");
-    //     scanf("%d", &n);
-    //     double tabA[n], tabB[n], tabC[n];
-    //     A(tabA, n);
-    //     B(tabB, n);
-    //     C(tabA, tabB, tabC, n);
-    //     wypisanie(tabA, tabB, tabC, n);
-    // }
-    //
+    // a
+    {
+        int n;
+        printf("Ile chcesz wyrazow w tablicy: ");
+        scanf("%d", &n);
+
+        double A[n];
+        for (int i=0; i<n; i++) {
+            printf("Podaj %d wyraz tablicy: ", (i+1));
+            scanf("%lg", &A[i]);
+        }
+        printf("A = {");
+        for (int i=0; i<n; i++) {
+            printf("%g", A[i]);
+            if (i<n-1) {
+                printf(", ");
+            }
+        }
+        printf("}\n");
+
+        double B[n];
+        srand(time(0));
+        printf("B = {");
+        for (int i=0; i<n; i++) {
+            B[i]=(double)rand()/RAND_MAX*100;
+            printf("%g", B[i]);
+            if (i<n-1) {
+                printf(", ");
+            }
+        }
+        printf("}\n");
+
+        double C[n];
+        printf("C = {");
+        for (int i=0; i<n; i++) {
+            C[i]=A[i]*B[i];
+            printf("%g", C[i]);
+            if (i<n-1) {
+                printf(", ");
+            }
+        }
+        printf("}\n\n");
+    }
+
+    // b
+    {
+        int D[10][3];
+        int silnia(int n);
+        int fib(int n);
+        printf("n        SIL  FIB\n");
+        for (int i=0; i<10; i++) {
+            D[i][0]=i+1;
+            D[i][1]=silnia(i);
+            D[i][2]=fib(i);
+            }
+
+        for (int i=0; i<10; i++){
+            printf("%-2d  %8d %4d\n", D[i][0], D[i][1], D[i][2]);
+        }
+        printf("\n");
+    }
+
+
+    // c
+    {
+        void A(double A[], int n);
+        void B(double B[], int n);
+        void C(double A[], double B[], double C[], int n);
+        void wypisanie(double A[], double B[], double C[], int n);
+        int n;
+        printf("Ile chcesz wyrazow w tablicy: ");
+        scanf("%d", &n);
+        double tabA[n], tabB[n], tabC[n];
+        A(tabA, n);
+        B(tabB, n);
+        C(tabA, tabB, tabC, n);
+        wypisanie(tabA, tabB, tabC, n);
+    }
+
     // d
-    // {
-    //     int pot_liczby(int a, int n);
-    //     int a, n;
-    //     printf("Podaj a: ");
-    //     scanf("%d",&a);
-    //     printf("Podaj n: ");
-    //     scanf("%d",&n);
-    //     printf("%d", pot_liczby(a, n));
-    // }
+    {
+        int pot_liczby(int a, int n);
+        int a, n;
+        printf("Podaj a: ");
+        scanf("%d",&a);
+        printf("Podaj n: ");
+        scanf("%d",&n);
+        printf("%d", pot_liczby(a, n));
+    }
 
     // Zadanie dodatkowe
     {
